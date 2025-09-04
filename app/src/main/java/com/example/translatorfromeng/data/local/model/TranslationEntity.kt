d0b7f99e937efc58ef04a6dd90db4e34ec3ed7cf
Kris <blackphoenix134@mail.ru>
2025-09-04 10:53:14 +0300

    feat(data): add unique index for englishWord
    Update TranslationEntity with unique index on englishWord to
    prevent duplicates.

--- app/src/main/java/com/example/translatorfromeng/data/local/model/TranslationEntity.kt
+++ app/src/main/java/com/example/translatorfromeng/data/local/model/TranslationEntity.kt
@@ -1,9 +1,10 @@
 package com.example.translatorfromeng.data.local.model
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "translation")
+@Entity(tableName = "translation", indices = [Index(value = ["englishWord"], unique = true)])
 data class TranslationEntity(
     @PrimaryKey(autoGenerate = true) val id: Long = 0,
     val englishWord: String,
